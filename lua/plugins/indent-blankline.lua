6f5f8facd96a6e2408184bed02e00e0b5708ce87
Ryan Cobb <rcobb@gitlab.com>
2021-12-03 14:45:13 -0800

    configure indent-blankline

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,4 +1,6 @@
 require('indent_blankline').setup {
+  use_treesitter = true,
   show_current_context = true,
-  show_current_context_start = true
+  show_current_context_start = true,
+  filetype_exclude = { 'toggleterm' }
 }
