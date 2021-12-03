ca7250301f537f633123566dacb8dba2f2209194
Ryan Cobb <rcobb@gitlab.com>
2021-12-03 12:44:49 -0800

    configure toggleterm

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,7 +7,10 @@ vim.api.nvim_set_keymap('n', '<leader>e', ':lua require("nvimtree").toggle()<CR>
 vim.api.nvim_set_keymap('n', '<leader>c', ':BufferClose<CR>', opts)
 vim.api.nvim_set_keymap('n', '<leader>f', ':Telescope find_files<CR>', opts)
 vim.api.nvim_set_keymap('n', '<leader>t', ':Telescope live_grep<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>sr', ':Telescope oldfiles<CR>', opts)
 vim.api.nvim_set_keymap('n', '<leader>h', ':noh<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>tl', ":let @*=join([expand('%'), line('.')], ':')<CR>", opts)
+vim.api.nvim_set_keymap('n', '<leader>tf', ":let @*=expand('%')<CR>", opts)
 
 vim.api.nvim_set_keymap('n', '<tab>', ':BufferNext<CR>', opts)
 vim.api.nvim_set_keymap('n', '<s-tab>', ':BufferPrevious<CR>', opts)
@@ -26,9 +29,16 @@ vim.api.nvim_set_keymap('n', '<c-right>', ':vertical resize +2<CR>', opts)
 vim.api.nvim_set_keymap('x', '<s-k>', ":move '<-2<CR>gv-gv", opts)
 vim.api.nvim_set_keymap('x', '<s-j>', ":move '>+1+<CR>gv-gv", opts)
 
+-- Terminal
 vim.api.nvim_set_keymap('t', '<c-h>', '<c-\\><c-n><c-w>h', opts)
 vim.api.nvim_set_keymap('t', '<c-j>', '<c-\\><c-n><c-w>j', opts)
 vim.api.nvim_set_keymap('t', '<c-k>', '<c-\\><c-n><c-w>k', opts)
 vim.api.nvim_set_keymap('t', '<c-l>', '<c-\\><c-n><c-w>l', opts)
 
 vim.api.nvim_set_keymap('t', 'jk', '<C-\\><C-n>', opts)
+
+vim.api.nvim_set_keymap('n', [[<c-\>]], '<cmd>exe v:count1 . "ToggleTerm direction=vertical"<CR>', opts)
+vim.api.nvim_set_keymap('i', [[<c-\>]], '<esc><cmd>exe v:count1 . "ToggleTerm direction=vertical"<CR>', opts)
+vim.api.nvim_set_keymap('n', [[<c-t>]], '<cmd>exe v:count1 . "ToggleTerm direction=horizontal"<CR>', opts)
+vim.api.nvim_set_keymap('i', [[<c-t>]], '<esc><cmd>exe v:count1 . "ToggleTerm direction=horizontal"<CR>', opts)
+
