9c362703974c34372609e57c85922881f9f5ec18
Ryan Cobb <rcobb@gitlab.com>
2021-12-15 16:49:37 -0800

    ignore indent lsp-installer

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -2,5 +2,5 @@ require('indent_blankline').setup {
   use_treesitter = false,
   show_current_context = false,
   show_current_context_start = false,
-  filetype_exclude = { 'toggleterm', 'packer' }
+  filetype_exclude = { 'lsp-installer', 'toggleterm', 'packer' }
 }
