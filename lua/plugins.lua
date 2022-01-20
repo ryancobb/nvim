32d8fdc66dc192e3dc32057b203433e18e296f8b
Ryan Cobb <rcobb@gitlab.com>
2022-01-20 11:00:07 -0800

    add back vim-kitty-navigattor

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -75,6 +75,11 @@ return require('packer').startup({function(use)
   }
 
   use {
+    'knubie/vim-kitty-navigator',
+    run = 'cp ./*.py ~/.config/kitty/'
+  }
+
+  use {
     'RRethy/vim-illuminate',
     config = function() require('plugins.vim-illuminate') end
   }
