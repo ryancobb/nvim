b55520b03a7f30907147bf9898e7a9025f9763f5
Ryan Cobb <rcobb@gitlab.com>
2022-01-28 16:02:54 -0800

    fix lsp keybindings

--- lua/plugins/configs/fzf-lua.lua
+++ lua/plugins/configs/fzf-lua.lua
@@ -1,1 +1,5 @@
-require('fzf-lua').setup {}
+require('fzf-lua').setup {
+  files = {
+    fd_opts = "--color=never --type f --hidden --follow --exclude .git --exclude spec/fixtures --exclude vendor --exclude qa/vendor"
+  }
+}
