1e9ab8b25e2b60a4bc8c55dc6b9fca7e9d6321c8
Ryan Cobb <rcobb@gitlab.com>
2022-01-31 13:12:29 -0800

    configure fzf-lua

--- lua/plugins/configs/fzf-lua.lua
+++ lua/plugins/configs/fzf-lua.lua
@@ -1,5 +1,9 @@
 require('fzf-lua').setup {
+  global_resume = false,
   files = {
     fd_opts = "--color=never --type f --hidden --follow --exclude .git --exclude spec/fixtures --exclude vendor --exclude qa/vendor"
+  },
+  old_files = {
+    cwd_only = true
   }
 }
