5c48f84a2245ede8b179899f14bcbfbdd3e9d249
Ryan Cobb <rcobb@gitlab.com>
2022-02-14 16:48:58 -0800

    fix fzf live_grep_glob

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -15,7 +15,7 @@ local keymaps = {
     {'<C-l>', '<C-w>l'},
 
     {'<leader>f', ':FzfLua files<CR>'},
-    {'<leader>st', ':lua require("fzf-lua").live_grep_glob<CR>'},
+    {'<leader>st', ':lua require("fzf-lua").live_grep_glob()<CR>'},
     {'<leader>ss', ':lua require("spectre").open()<CR>'},
     {'<leader>sc', ':FzfLua grep_cword<CR>'},
     {'<leader>sC', ':FzfLua grep_cWORD<CR>'},
