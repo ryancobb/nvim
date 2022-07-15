877c6fa0e71c973ca0ad9bbd1bb6e6d4f6332194
Ryan Cobb <ryan.cobb@hey.com>
2022-07-15 15:26:40 -0700

    add swift.vim

--- init.lua
+++ init.lua
@@ -112,6 +112,7 @@ require('packer').startup(function(use)
   use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install",
     setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
   use 'jinh0/eyeliner.nvim'
+  use 'keith/swift.vim'
 end)
 
 ------------------------------------------------------------------------------------------------------------------------------------
