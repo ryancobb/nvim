5ac98ce2537fe71f16889f6776b96c63543c8902
Ryan Cobb <rcobb@gitlab.com>
2024-01-18 11:37:29 -0800

    limit cmp menu width

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -4,6 +4,31 @@ return {
     opts = function(_, opts)
       local cmp = require("cmp")
 
+      opts.formatting = {
+        format = function(_entry, item)
+          local icons = require("lazyvim.config").icons.kinds
+          local content = item.abbr
+          local menu = item.menu
+          local win_width = vim.api.nvim_win_get_width(0)
+          local max_content_width = math.floor(win_width * 0.3)
+          local max_menu_width = math.floor(win_width * 0.2)
+
+          if icons[item.kind] then
+            item.kind = icons[item.kind] .. item.kind
+          end
+
+          if #content > max_content_width then
+            item.abbr = vim.fn.strcharpart(content, 0, max_content_width - 3) .. "..."
+          end
+
+          if (menu ~= nil and #menu > max_menu_width) then
+            item.menu = vim.fn.strcharpart(menu, 0, max_menu_width - 3) .. "..."
+          end
+
+          return item
+        end,
+      }
+
       opts.mapping = vim.tbl_deep_extend("force", opts.mapping, {
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
         ["<Right>"] = cmp.mapping.confirm({ select = false }),
