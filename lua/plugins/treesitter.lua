318d95901a5ddb936c4883c1124c59170029bc3b
Ryan Cobb <rcobb@gitlab.com>
2024-03-29 12:49:42 -0700

    Remove aerial in favor of trouble

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,15 +2,9 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
-      indent = {
-        enable = false,
-      },
-      endwise = {
-        enable = true,
-      },
-      matchup = {
-        enable = true,
-      },
+      indent = { enable = false },
+      endwise = { enable = true },
+      matchup = { enable = true },
       ensure_installed = {
         "bash",
         "c",
@@ -36,6 +30,11 @@ return {
         "vimdoc",
         "yaml",
       },
+      textobjects = {
+        move = {
+          enable = false,
+        },
+      },
     },
   },
   { "RRethy/nvim-treesitter-endwise" },
@@ -43,7 +42,16 @@ return {
     "nvim-treesitter/nvim-treesitter-context",
     branch = "rc/ruby_do_block",
     opts = {
-      max_lines = 0
-    }
+      max_lines = 0,
+    },
+    keys = {
+      {
+        "[c",
+        function()
+          require("treesitter-context").go_to_context(vim.v.count1)
+        end,
+        "jump to context",
+      },
+    },
   },
 }
