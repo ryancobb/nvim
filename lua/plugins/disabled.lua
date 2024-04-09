e9269d809f7610e79b2c0626bbb605f656ccb6a3
Ryan Cobb <rcobb@gitlab.com>
2024-04-09 11:25:34 -0700

    remove dashboard, use mini-starter

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -5,4 +5,5 @@ return {
   { "folke/tokyonight.nvim", enabled = false },
   { "nvim-pack/nvim-spectre", enabled = false },
   { "nvim-telescope/telescope.nvim", enabled = false },
+  { "nvimdev/dashboard-nvim", enabled = false }
 }
