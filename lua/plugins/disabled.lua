5a555e3a5e70c5252ce4aa561d1c17730da93f62
Ryan Cobb <rcobb@gitlab.com>
2024-07-12 14:24:10 -0700

    sync

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -4,6 +4,6 @@ return {
   { "folke/todo-comments.nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
   { "nvim-pack/nvim-spectre", enabled = false },
-  { "nvim-telescope/telescope.nvim", enabled = false },
+  -- { "nvim-telescope/telescope.nvim", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false }
 }
--- lua/plugins/grapple.lua
+++ lua/plugins/grapple.lua
@@ -1,8 +1,5 @@
 return {
   "cbochs/grapple.nvim",
-  dependencies = {
-    { "nvim-tree/nvim-web-devicons", lazy = true },
-  },
   opts = {
     scope = "git_branch",
     icons = true,
