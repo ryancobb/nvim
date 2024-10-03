4bb448b373b1353a503f409c06f3816524e97279
Ryan Cobb <rcobb@gitlab.com>
2024-10-03 16:40:25 -0700

    conform - bundle exec rubocop

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,7 +3,10 @@ return {
   opts = {
     formatters_by_ft = {
       javascript = { "prettier" },
-      ruby = { "rubocop" },
+      ruby = {
+        command = "bundle",
+        args = "exec rubocop",
+      },
       vue = { "prettier" },
       markdown = { "markdownlint-cli2" }
     },
